0736e399e50408f148b6eac5abc19ac263f109b6
Felipe <felipemalentaq@hotmail.com>
2023-06-13 17:58:02 -0300

    requisito 2 ok

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith('txt'):
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, mode='r') as file:
+            retorno = file.read()
+            # print('Leitura: ', retorno.split('\n'))
+            return retorno.split("\n")
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        # print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
