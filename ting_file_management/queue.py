69441a85476aa8336775c9313ed458e86069c7ad
Felipe <felipemalentaq@hotmail.com>
2023-06-13 15:24:01 -0300

    requisito 1 ok

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,49 @@
 from ting_file_management.abstract_queue import AbstractQueue
+# from collections import deque
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
+
+    def is_empty(self):
+        return not bool(self.__len__())
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # if self.is_empty():
+        #     raise IndexError("Índice Inválido ou Inexistente")
+
+        if self.__len__() > index >= 0:
+            return self.queue[index]
+
+        raise IndexError("Índice Inválido ou Inexistente")
+
+    # def __str__(self):
+    #     return f'''
+    #     tamanho: {self.__len__}
+    #     lista: {self.queue}
+    #     '''
+
+
+# listaNome = Queue()
+# print('1: ', listaNome.search(0))
+# listaNome.enqueue('Felipe')
+# print('2: ', listaNome)
+
+# listaNome.enqueue('Bruno')
+# print('3: ', listaNome.search)
+
+# listaNome.dequeue()
+# print('4: ', listaNome.search)
+# print('5', listaNome.is_empty())
