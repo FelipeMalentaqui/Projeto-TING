b35aa49f383fdc4b687c19cb74da4a6ac6ece120
Felipe <felipemalentaq@hotmail.com>
2023-06-14 16:23:50 -0300

    requisito 7 e 8 em andamento

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,38 +4,7 @@ from ting_file_management.queue import Queue
 
 def exists_word(word: str, instance: Queue):
     return list()
-    # conteudo = instance
-    # arquivos = ''
-    # texto_palvra = ''
-    # linha_de_palavras = []
-    # quantidade_palavra = []
-
-    # if len(conteudo.queue) == 0:
-
-    # for arquivo in conteudo.queue:
-    #     arquivos = arquivo['nome_do_arquivo']
-    #     texto_palvra = arquivo['linhas_do_arquivo']
-
-    # print('NOME_ARQUIVO: ', arquivos)
-    # print('TEXTO_FORA: ', texto_palvra)
-
-    # for word in texto_palvra:
-    #     texto_palvra.split()
-    #     quantidade_palavra.append(texto_palvra.count(word))
-
-    # print('QUANTIDADE: ', quantidade_palavra)
-    # print('SEPARA: ', texto_palvra)
-    # print('LINHAS: ', linha_de_palavras)
-
-    # print('TEXTO_FORA2 : ', texto_palvra)
-
-    # formato_arquivo = {
-    #     "palavra": word,
-    #     "arquivo": arquivos,
-    #     "ocorrencias": linha_de_palavras
-    # }
-    # return ('FORMATO: ', formato_arquivo)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return list()
